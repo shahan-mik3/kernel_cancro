c7d5afb7034a36fd8490aac97693111078a5bcf1
Kamal Negi <kamaln@codeaurora.org>
2016-12-21 17:12:07 +0530

    radio-iris: Fix bit masking for program service
    Use correct bit masking for program service enable in RDS group
    processing.
    
    Change-Id: I3dcfc401783dedbbf11d72028eac7e58a5aa893b
    Signed-off-by: Kamal Negi <kamaln@codeaurora.org>

--- drivers/media/radio/radio-iris.c
+++ drivers/media/radio/radio-iris.c
@@ -4337,8 +4337,7 @@ static int iris_vidioc_s_ctrl(struct file *file, void *priv,
 		break;
 	case V4L2_CID_PRIVATE_IRIS_RDSGROUP_PROC:
 		saved_val = radio->g_rds_grp_proc_ps;
-		rds_grps_proc = radio->g_rds_grp_proc_ps | ctrl->value;
-		radio->g_rds_grp_proc_ps = (rds_grps_proc >> RDS_CONFIG_OFFSET);
+		radio->g_rds_grp_proc_ps |= ctrl->value;
 		retval = hci_fm_rds_grps_process(
 				&radio->g_rds_grp_proc_ps,
 				radio->fm_hdev);
@@ -4352,7 +4351,7 @@ static int iris_vidioc_s_ctrl(struct file *file, void *priv,
 		break;
 	case V4L2_CID_PRIVATE_IRIS_PSALL:
 		saved_val = radio->g_rds_grp_proc_ps;
-		rds_grps_proc = (ctrl->value << RDS_CONFIG_OFFSET);
+		rds_grps_proc = (ctrl->value << RDS_PS_OFFSET);
 		radio->g_rds_grp_proc_ps |= rds_grps_proc;
 		retval = hci_fm_rds_grps_process(
 				&radio->g_rds_grp_proc_ps,
