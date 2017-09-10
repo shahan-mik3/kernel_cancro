0f71f1752ae67ac462257dfa380753ace6921b2a
Vasishath Kaushal <vasishath@gmail.com>
2017-09-10 13:44:02 +0530

    We have a 2A charger

--- drivers/usb/dwc3/dwc3_otg.h
+++ drivers/usb/dwc3/dwc3_otg.h
@@ -22,8 +22,8 @@
 #include <linux/usb/otg.h>
 #include "power.h"
 
-#define DWC3_IDEV_CHG_MAX 1500
-#define DWC3_HVDCP_CHG_MAX 1800
+#define DWC3_IDEV_CHG_MAX 2000
+#define DWC3_HVDCP_CHG_MAX 2000
 
 /*
  * Module param to override current drawn for DCP charger
