2c6d22bbe153151b2a7690845f277fe7032708a1
Vasishath Kaushal <vasishath@gmail.com>
2017-12-18 19:34:55 +0530

    build-script: Switch to gcc 4.9

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,12 @@
 DEFCONFIG=cyanogen_cancro_defconfig
 OBJ_DIR=`pwd`/obj
-TOOLCHAIN=${HOME}/toolchain/arm-eabi-4.8/bin/arm-eabi-
+TOOLCHAIN=${HOME}/toolchain/arm-linux-androideabi-4.9/bin/arm-linux-androidkernel-
 make ARCH=arm O=$OBJ_DIR CROSS_COMPILE=${TOOLCHAIN} $DEFCONFIG
 make -j32 ARCH=arm O=$OBJ_DIR CROSS_COMPILE=${TOOLCHAIN}
 rm -f ~/AnyKernel2/zImage*
 rm -f ~/AnyKernel2/dtb*
 cp $OBJ_DIR/arch/arm/boot/zImage-dtb ~/AnyKernel2/
+cp $OBJ_DIR/drivers/staging/prima/wlan.ko ~/AnyKernel2/modules/
 cd ~/AnyKernel2
 DATE=$(date +"%m-%d-%y")
 rm *.zip
