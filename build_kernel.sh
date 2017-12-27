19e37b37ced63ab43a1b64f32221f1885e768aa1
Vasishath Kaushal <vasishath@gmail.com>
2017-12-28 00:28:31 +0530

    Revert "build-script: Switch to gcc 4.9"
    This reverts commit e43eb4aaddd223a7ac960349ed6fdcedef1a6147.

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,12 +1,11 @@
 DEFCONFIG=cyanogen_cancro_defconfig
 OBJ_DIR=`pwd`/obj
-TOOLCHAIN=${HOME}/toolchain/arm-linux-androideabi-4.9/bin/arm-linux-androidkernel-
+TOOLCHAIN=${HOME}/toolchain/arm-eabi-4.8/bin/arm-eabi-
 make ARCH=arm O=$OBJ_DIR CROSS_COMPILE=${TOOLCHAIN} $DEFCONFIG
 make -j32 ARCH=arm O=$OBJ_DIR CROSS_COMPILE=${TOOLCHAIN}
 rm -f ~/AnyKernel2/zImage*
 rm -f ~/AnyKernel2/dtb*
 cp $OBJ_DIR/arch/arm/boot/zImage-dtb ~/AnyKernel2/
-cp $OBJ_DIR/drivers/staging/prima/wlan.ko ~/AnyKernel2/modules/
 cd ~/AnyKernel2
 DATE=$(date +"%m-%d-%y")
 rm *.zip
